17f1a36612c001fc2f21ceaf85ab9a0a77c721b2
Onurhann Aytac <onurhan.aytac@obss.com.tr>
2022-04-14 01:23:24 +0300

    lint fixes

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -12,20 +12,22 @@ const Map: React.FC<MapProps> = ({
   onLoad,
   markerLocations,
 }: MapProps) => {
-  console.log("render");
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyCPz9UEVtZRSDZv5XRwdXuYFef3xqYK1Og",
   });
 
   const onLoadMap = useCallback((_map: google.maps.Map) => {
-    console.log("render2");
     _map.fitBounds(new window.google.maps.LatLngBounds());
     if (!onLoad) return;
 
     onLoad(_map);
     setTimeout(() => {
       _map.setZoom(16);
+      _map.panTo({
+        lat: 48.13598,
+        lng: 11.58643,
+      });
     }, 500);
   }, []);
 
